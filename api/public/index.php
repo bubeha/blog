52c344e4963ba0ef86adc75eed1bc41d76194920
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-01-14 23:09:04 +0300

    chore: update default settings

--- api/public/index.php
+++ api/public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Kernel;
+declare(strict_types=1);
+
+use App\Shared\Infrastructure\Symfony\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
+return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
