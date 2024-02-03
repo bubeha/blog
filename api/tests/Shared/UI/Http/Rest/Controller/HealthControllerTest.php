2e7e4cf4a7699ee2a7f8d07813cbe93d1188c399
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-02-03 21:20:12 +0300

    test: add test for the HealthControllerTest.php

---
+++ api/tests/Shared/UI/Http/Rest/Controller/HealthControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Shared\UI\Http\Rest\Controller;
+
+use App\Shared\UI\Http\Rest\Controller\HealthController;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class HealthControllerTest extends WebTestCase
+{
+    public function testHealthAction(): void
+    {
+        $client = self::createClient();
+
+        $client->request('GET', '/health');
+
+        self::assertResponseIsSuccessful();
+        self::assertSame('{"status":"alive"}', $client->getResponse()->getContent());
+    }
+}
