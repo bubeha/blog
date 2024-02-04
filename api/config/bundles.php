c01eefaae3b403cf463100012f32bd1b852b8619
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-02-04 13:43:04 +0300

    chore: add symfony/security package

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-
 return [
-    FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
